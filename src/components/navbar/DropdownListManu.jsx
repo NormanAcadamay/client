871d9fc087ebd7c4aea62bc7790fe299f75ecf90
NormanAcadamay <korawit4zx@gmail.com>
2025-09-07 22:48:42 +0700

    Update:Ep5-Mac

--- src/components/navbar/DropdownListManu.jsx
+++ src/components/navbar/DropdownListManu.jsx
@@ -1,5 +1,5 @@
 // rafce
-import React from 'react'
+import React from "react";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -7,45 +7,73 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+} from "@/components/ui/dropdown-menu";
 import { AlignLeft } from "lucide-react";
-import UserIcon from './UserIcon';
-import { Button } from '../ui/button';
-import { links } from '@/utils/links';
-import { Link } from 'react-router';
-
+import UserIcon from "./UserIcon";
+import { Button } from "../ui/button";
+import { links } from "@/utils/links";
+import { Link } from "react-router";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
 const DropdownListManu = () => {
   return (
     <DropdownMenu>
-  <DropdownMenuTrigger asChild>
-<Button variant="outline">
-<AlignLeft />
-<UserIcon/>
-</Button>
-  </DropdownMenuTrigger>
-  <DropdownMenuContent>
-    <DropdownMenuLabel>My Account.</DropdownMenuLabel>
-    <DropdownMenuSeparator />
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline">
+          <AlignLeft />
+          <UserIcon />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
 
-    {
-        links.map((item,index)=>{
-            // code body javascript
-            // return 1 element
-            // console.log(item.href)
-            return (
-                <DropdownMenuItem key={index}>
-                   <Link to={item.href}>{item.Label}</Link> 
-                    </DropdownMenuItem>
-            )
-            
+        {links.map((item, index) => {
+          // code body javascript
+          // return 1 element
+          // console.log(item.href)
+          return (
+            <DropdownMenuItem key={index}>
+              <Link to={item.href}>{item.Label}</Link>
+            </DropdownMenuItem>
+          );
+        })}
 
-        })
-    }
-    
- 
-  </DropdownMenuContent>
-</DropdownMenu>
+        <DropdownMenuSeparator />
+        <SignedOut>
+          <DropdownMenuItem>
+            {/* กรณียังไม่ได้ล็อกอิน */}
+
+            <SignInButton mode="modal">
+              <button>Login</button>
+            </SignInButton>
+          </DropdownMenuItem>
+
+          <DropdownMenuItem>
+            <SignUpButton mode="modal">
+              <button>Register</button>
+            </SignUpButton>
+          </DropdownMenuItem>
+        </SignedOut>
+
+           {/* กรณีที่ล็อคอินแล้ว */}
+       <SignedIn>
+        <DropdownMenuItem>
+        {/* <UserButton/> */}
+        <SignOutButton/>
+        </DropdownMenuItem>
+       </SignedIn>
+
+
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,16 @@
 // rafce
 import React from 'react'
+// import { SignedIn, SignedOut, SignInButton, SignOutButton, SignUpButton, UserButton } from '@clerk/clerk-react';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      Home
+    
+
+    
+
+      </div>
   )
 }
 
